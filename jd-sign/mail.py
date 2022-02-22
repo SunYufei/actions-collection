91ad2286c2fa9af969a6473d984a1e6e999b9962
Sun <sunyufei963@qq.com>
2022-02-22 14:08:33 +0800

    Update mail.py

--- jd-sign/mail.py
+++ jd-sign/mail.py
@@ -1,26 +1,27 @@
-from email.header import Header
-from email.mime.multipart import MIMEMultipart
+# from email.header import Header
+# from email.mime.multipart import MIMEMultipart
 from pathlib import Path
-import argparse
-import smtplib
+# import argparse
+# import smtplib
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file')
-parser.add_argument('-s', '--sender')
-parser.add_argument('-r', '--receiver')
-parser.add_argument('--host')
-parser.add_argument('-u', '--username')
-parser.add_argument('-p', '--password')
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-f', '--file')
+# parser.add_argument('-s', '--sender')
+# parser.add_argument('-r', '--receiver')
+# parser.add_argument('--host')
+# parser.add_argument('-u', '--username')
+# parser.add_argument('-p', '--password')
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
-file = Path(args.file)
-sender = args.sender
-receiver = args.receiver
-host = args.host
-port = 465
-username = args.username
-password = args.password
+file = Path('./result.txt')
+# file = Path(args.file)
+# sender = args.sender
+# receiver = args.receiver
+# host = args.host
+# port = 465
+# username = args.username
+# password = args.password
 
 need_send = False
 with file.open() as f:
@@ -30,16 +31,17 @@ with file.open() as f:
             need_send = True
 
 if need_send:
-    smtp = smtplib.SMTP_SSL(host=host)
-    smtp.connect(host, port)
-    smtp.login(username, password)
+    exit(-1)
+#     smtp = smtplib.SMTP_SSL(host=host)
+#     smtp.connect(host, port)
+#     smtp.login(username, password)
 
-    try:
-        msg = MIMEMultipart()
-        msg['From'] = sender
-        msg['To'] = receiver
-        msg['Subject'] = Header('JD-Sign Cookie needs refresh', 'utf-8')
+#     try:
+#         msg = MIMEMultipart()
+#         msg['From'] = sender
+#         msg['To'] = receiver
+#         msg['Subject'] = Header('JD-Sign Cookie needs refresh', 'utf-8')
 
-        smtp.sendmail(sender, receiver, msg.as_bytes())
-    except Exception as e:
-        print(e)
+#         smtp.sendmail(sender, receiver, msg.as_bytes())
+#     except Exception as e:
+#         print(e)
